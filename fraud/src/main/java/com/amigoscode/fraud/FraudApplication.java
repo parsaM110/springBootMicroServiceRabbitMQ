e77df9944412ac3ed606d86ffae380d7041cd543
parsaM110 <pmomeni80@gmail.com>
2023-11-04 12:04:49 +0330

    registering Fraud to Eureka-server / some minor modification in yml of Customer too

--- fraud/src/main/java/com/amigoscode/fraud/FraudApplication.java
+++ fraud/src/main/java/com/amigoscode/fraud/FraudApplication.java
@@ -2,9 +2,11 @@ package com.amigoscode.fraud;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 @SpringBootApplication
+@EnableEurekaClient
 public class FraudApplication {
 public static void main(String[] args) {
     SpringApplication.run(FraudApplication.class, args);
