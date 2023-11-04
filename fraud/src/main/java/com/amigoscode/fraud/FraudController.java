8607da80b6508e5c41dc2d7c96030c855f4a218c
parsaM110 <pmomeni80@gmail.com>
2023-11-04 11:00:05 +0330

    adding logging for FraudController

--- fraud/src/main/java/com/amigoscode/fraud/FraudController.java
+++ fraud/src/main/java/com/amigoscode/fraud/FraudController.java
@@ -1,11 +1,13 @@
 package com.amigoscode.fraud;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/fraud-check")
 @AllArgsConstructor
+@Slf4j
 public class FraudController {
 
     private final FraudCheckService fraudCheckService;
@@ -16,6 +18,7 @@ public class FraudController {
             @PathVariable("customerId") Integer customerId) {
 
         boolean isFraudulentCustomer = fraudCheckService.isFraudulentCustomer(customerId);
+        log.info("Is customer fraudulent: {} {}", customerId, isFraudulentCustomer);
         return new FraudCheckResponse(isFraudulentCustomer);
 
     }
